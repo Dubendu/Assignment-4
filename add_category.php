855e323d583ddcac133a9e501b954f938b56d68e
Dubendu <singhdubendu222@gmail.com>
2021-08-11 17:00:25 -0400

    modify add_category.php

--- add_category.php
+++ add_category.php
@@ -1,24 +1,12 @@
+<?php include('config.php')?>
 <?php
-// server connection
-$servername = "localhost";
-$username = "root";
-// their is no password in default
-$password = "";
-// our database name
-$dbname = "category";
-  // got input from html using '$_POST[]'
-   $category_name = $_POST['c_name'];
-     // sql connection
-     if($category_name == "" ){
-         die("Please enter the feilds!");
-     }
-  // lets check its connected
-  $conn = new mysqli($servername, $username, $password,$dbname);
+
+  $category_name = $_POST['c_name'];
+    // sql connection
+    if($category_name == "" ){
+        die("Please enter the feilds!");
+    }
   
-  // Check connection
-  if ($conn->connect_error) {
-      die("Connection failed: " . $conn->connect_error);
-  } 
   // in php we use '.' operator for string concatenation!
   // store sql query in variable
   // we set id as auto increment so set it as empty
