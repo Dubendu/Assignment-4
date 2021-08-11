ebf864c4f0758481e06795d17fcdccbbf28fe41b
Dubendu <singhdubendu222@gmail.com>
2021-08-11 11:26:13 -0400

    modified update.php

--- update.php
+++ update.php
@@ -52,6 +52,11 @@ else{
 ?>
 <html>
 <head>
+     <meta charset="utf-8">
+    <meta name="description" content="add category">
+    <meta name="Keywords" content="add,categories">
+    <meta name="author" content="Dubendu">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Category</title>
     <meta charset="UTF-8">
     <meta name="description" content="add category">
