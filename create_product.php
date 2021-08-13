9e3a092d4f8fbac633d6319acb25c51c357d2c35
Dubendu <singhdubendu222@gmail.com>
2021-08-13 18:11:45 -0400

    modify create product

--- create_product.php
+++ create_product.php
@@ -47,7 +47,7 @@ else{
         </nav>
     </header>
     <br>
-    <form method="post" id="regForm" onsubmit="return validateForm()" class="create_product_form" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data" >
+    <form method="post" id="regForm" class="create_product_form" action="<?php echo $_SERVER['PHP_SELF'];?>" enctype="multipart/form-data" >
         <div class="container">
             <h1>Add Product</h1>
             <div class="input-group">
