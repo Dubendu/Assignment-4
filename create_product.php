5086a6e126b1d666dfa503ed52c60fcb2ef3e932
Dubendu <singhdubendu222@gmail.com>
2021-08-13 17:17:06 -0400

    modify create product file

--- create_product.php
+++ create_product.php
@@ -28,7 +28,6 @@ else{
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <link rel="stylesheet" href="CSS/product_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <script type="text/javascript">validateform();</script>
 </head>
 <body>
     <header>
