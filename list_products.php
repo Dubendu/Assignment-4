db8ab9c398d07dd9f4ace2076f258d60221e0ef3
Dubendu <singhdubendu222@gmail.com>
2021-08-13 13:17:42 -0400

    add list products

--- list_products.php
+++ list_products.php
@@ -7,7 +7,7 @@
     <meta name="Keywords" content="add,categories">
     <meta name="author" content="Dubendu">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/product_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
