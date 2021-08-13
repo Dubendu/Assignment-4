7a59d57ac60abf77a821114ce88a245ff14435b5
Dubendu <singhdubendu222@gmail.com>
2021-08-13 13:16:41 -0400

    add update product file

--- edit_product.php
+++ edit_product.php
@@ -48,7 +48,7 @@ else{
     <meta name="author" content="Dubendu">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <title>Update Category</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="CSS/product_style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
